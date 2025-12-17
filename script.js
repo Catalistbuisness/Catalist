92f387a7a5359239fe162ddceace97009c817d6e
Catalistbuisness <folashaquana@gmail.com>
2025-12-17 18:32:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function subscribe(e) {
     document.getElementById('subscribe-msg').textContent = 'Thank you for subscribing, ' + email + '!';
     document.getElementById('email').value = '';
 }
+function toggleCart() {
+    const cart = document.getElementById("cart");
+
+    if (cart.style.display === "none") {
+        cart.style.display = "block";
+    } else {
+        cart.style.display = "none";
+    }
+}
